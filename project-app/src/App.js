4163e7fe7e18ac157e26291d1f9e3f767caaf17d
Natalia <natmaya98@gmail.com>
2019-07-03 14:38:30 -0500

    informe-dos

--- project-app/src/App.js
+++ project-app/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import {Form, Button, Nav, Col, Container, Row} from 'react-bootstrap'
-
 import './App.css';
 
 class App extends React.Component { 
